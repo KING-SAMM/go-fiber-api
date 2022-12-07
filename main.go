17654b36bb73ce84099459152aee2aac202a67e1
kcsamm <kcsamm11@gmail.com>
2022-12-07 23:58:22 +0100

    Updated main.go

--- main.go
+++ main.go
@@ -4,3 +4,14 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Welcome to studioeternal")
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", hello)
+
+	app.Listen(":3000")
+}
