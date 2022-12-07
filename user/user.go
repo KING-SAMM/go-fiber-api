82a055d200315774b365496aae744dfae44e9fcc
kcsamm <kcsamm11@gmail.com>
2022-12-08 00:00:06 +0100

    Updated main.go

--- user/user.go
+++ user/user.go
@@ -9,3 +9,9 @@ var err error
 
 const DNS = "root:admin@tcp(127.0.0.1:3306)/godb?charset=utf8"
 
+type User struct {
+	gorm.Model 
+	FirstName string	`json:"firstname"`
+	LastName string		`json:"lastname"`
+	Email string		`json:"email"`
+}
