ff5a1c304068ade7f9c9e3263cae0b688b52c1d6
kcsamm <kcsamm11@gmail.com>
2022-12-09 10:21:19 +0100

    Implemented DeleteUser and UpdateUser functionalities

--- user/user.go
+++ user/user.go
@@ -32,23 +32,50 @@ func GetUsers(c *fiber.Ctx) error {
 	DB.Find(&users)
 	return c.JSON(&users)
 }
+
 func GetUser(c *fiber.Ctx) error {
 	id := c.Params("id")
 	var user User
 	DB.Find(&user, id)
 	return c.JSON(&user)
 }
+
 func SaveUser(c *fiber.Ctx) error {
 	user := new(User)
+	// Get data from request body
 	if err := c.BodyParser(user); err != nil {
 		return c.Status(500).SendString(err.Error())
 	}
 	DB.Save(&user)
 	return c.JSON(&user)
 }
+
 func DeleteUser(c *fiber.Ctx) error {
-
+	id := c.Params("id")
+	var user User
+	DB.First(&user, id)
+	if user.Email == "" {
+		return c.Status(500).SendString("User not available")
+	}
+	DB.Delete(&user)
+	return c.SendString("User deleted successfully!")
 }
-func UpdateUser(c *fiber.Ctx) error {
 
+func UpdateUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	user := new(User)
+	DB.First(&user, id)
+
+	// If data does not exist in database
+	if user.Email == "" {
+		return c.Status(500).SendString("User not available")
+	}
+
+	// Get data from request body
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	DB.Save(&user)
+	return c.JSON(&user)
 }
